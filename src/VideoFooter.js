f1df320ec18628f9648171eb5af851d12a83e4a2
Alexa fato <alexafato@MacBook-Pro-Alexa.local>
2020-10-13 03:39:07 +0300

    140

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import './VideoFooter.css'
-import music from './static/music.svg'
+import record from './static/record.svg'
+import music from './static/musical-note.svg'
+import comment from './static/comment.svg'
+import heart from './static/heart.png'
+import arrow from './static/arrow.svg'
+import { Button } from '@material-ui/core';
+import AudiotrackIcon from '@material-ui/icons/Audiotrack';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import TextsmsIcon from '@material-ui/icons/Textsms';
+import ShareIcon from '@material-ui/icons/Share';
+import Ticker from 'react-ticker'
+
 
 function VideoFooter() {
     return (
         <div className='videoFooter'>
+            
            <div className='videoFooter__text'>
+         
                <h3>@alex</h3>
                <p>this is a description</p>
+               <div className='videoFooter__ticker'>
+             <AudiotrackIcon className='videoFooter__icon' fontSize="large"/>
+              <Ticker mode='smooth'>
+                  {({index}) => (
+                      <>
+                      <p>YOOOO@@@U</p>
+                      </>
+                  )}
+              </Ticker>
+              </div>
            </div>
-           <img className='videoFooter__record' src={music} alt='music'/>
+            <FavoriteIcon fontSize="large"/>
+            <TextsmsIcon fontSize="large"/>
+            <ShareIcon  fontSize="large"/>
+            <img className='videoFooter__record' src={record} alt='music'/>
+         
         </div>
     )
 }
